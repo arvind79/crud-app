d80bd2c398d958b0e9f8eb70f6d6073cd9dcfd36
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-09-06 02:46:25 +0530

    error in user-scheme, error in plugin line because autoIncrement is depreceated

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = 'http://localhost:8000/';
+const URL = 'http://localhost:8000';
 
 export const addUser = async (data) => {
   try {
--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ import express from 'express';
 import Connection from './database/db.js';
 import dotenv from 'dotenv';
 import Routes from './routes/route.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 dotenv.config();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', Routes);
 
